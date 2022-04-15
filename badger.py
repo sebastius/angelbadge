4a85023dd0d11b5f009aa4d5f0f8c3475bb9e28d
Sebastius <bas@knutselaar.net>
2022-04-15 12:50:24 +0200

    Update badger.py

--- badger.py
+++ badger.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+# Made by Bas Oort (twitter @basoort, github.com/sebastius)
+# Feel free to use this and hack it. 
+
 import cups
 import qrcode
 import sys
@@ -5,62 +9,38 @@ from PIL import Image
 from PIL import ImageFont
 from PIL import ImageDraw
 
-# Data to encode
+while True:
+    nickname = input("Enter nickname: ")
+    print ("gonna print: " + nickname)
 
-data = input("Enter nickname: ")
-print (data)
+    from aztec_code_generator import AztecCode
+    aztec_code = AztecCode("angel"+nickname,size=23,compact=True) #angels get a prefix for Logistics Warehouse
+    aztec_code.save('aztec_code.png', module_size=4, border=0)
 
-# Creating an instance of QRCode class
-qr = qrcode.QRCode(version = 1,
-                   box_size = 10,
-                   border = 3)
+    background = Image.open('background.png') # image size 1016 x 648
+    aztec = Image.open('aztec_code.png')
+    aztec = aztec.convert('RGBA')
+    aztec = aztec.resize((255,255), resample = Image.Dither.NONE)
+    aztec = aztec.rotate(60, expand=True,fillcolor=None)
 
-# Adding data to the instance 'qr'
-qr.add_data(data)
+    angelbadge = Image.new('RGBA', size=(1016, 648))
+    angelbadge.paste(background, (0,0))
+    angelbadge.paste(aztec, ((1016-102-250),50),aztec)
+    draw = ImageDraw.Draw(angelbadge)
 
-qr.make(fit = True)
-img = qr.make_image(fill_color = 'black',
-                    back_color = 'white')
+    font = ImageFont.truetype("./Saira-Regular.ttf", 150)
+    length,height=font.getsize(nickname)
 
-img.save('MyQRCode2.png')
+    if length > 1000: # if the nickname is too wide in the default font, scale it down
+        font = ImageFont.truetype("./Saira-Regular.ttf", int(150/length*1000))
+        length,height=font.getsize(nickname)
 
-from aztec_code_generator import AztecCode
-aztec_code = AztecCode(data)
-aztec_code.save('aztec_code.png', module_size=4, border=0)
+    draw.rectangle((0, 520-(height/2),1016,520+(height/2)), fill="white", outline=None)
+    draw.text((1016/2,520), nickname, fill="black", anchor="mm", font=font)
 
-achtergrond = Image.open('pasjeachterkant.png')
-#qrcodeplaatje = Image.open('MyQRCode2.png')
-qrcodeplaatje = Image.open('aztec_code.png')
-qrcodeplaatje = qrcodeplaatje.convert('RGBA')
-qrcodeplaatje = qrcodeplaatje.resize((255,255), resample = Image.BILINEAR)
-qrcodeplaatje = qrcodeplaatje.rotate(60, expand=True,fillcolor=None)
+    angelbadge.save("badge.png", "PNG")
 
-fff = Image.new('RGBA', qrcodeplaatje.size, (255,)*4)
-
-angelbadge = Image.new('RGBA', size=(1016, 648))
-#angelbadge = Image.new('RGB', size=(648, 1016))
-#achtergrond = achtergrond.rotate(90, expand=True)
-angelbadge.paste(achtergrond, (0,0))
-angelbadge.paste(qrcodeplaatje, ((1016-102-250),50),qrcodeplaatje)
-draw = ImageDraw.Draw(angelbadge)
-
-#draw.rectangle((0,420, 1016, 620), fill=(255,255,255), outline=None)
-
-font = ImageFont.truetype("./Saira-Regular.ttf", 150)
-length,height=font.getsize(data)
-print(length,height)
-
-if length > 1000:
-    font = ImageFont.truetype("./Saira-Regular.ttf", int(150/length*1000))
-    length,height=font.getsize(data)
-    print(length,height)
-
-draw.rectangle((0, 520-(height/2),1016,530+(height/2)), fill=(255,255,255), outline=None)
-draw.text(((1016-length)/2, 520-(height/2)), data, (0, 0, 0), font=font)
-
-angelbadge.save("badge.png", "PNG")
-
-pebble = cups.Connection()
-file = "./badge.png"
-
-#pebble.printFile ("Pebble", file, "angelbadge", {})
+    pebble = cups.Connection()
+    file = "./badge.png"
+    #pebble.printFile ("Pebble", file, "angelbadge", {}) #if you don't want to print a million cards during testing, comment this line
+    print("Done!")
