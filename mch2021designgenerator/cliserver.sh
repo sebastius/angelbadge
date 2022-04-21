5bb30a19f3eb44a066169ab9ca508517491c2b04
Sebastius <bas@knutselaar.net>
2022-04-21 23:39:37 +0200

    puinruimen

--- mch2021designgenerator/cliserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm -rf ./listen.sock
-node ./cli.mjs -s ./listen.sock -w 1016 -t 648 -b
--- mch2021designgenerator/node_modules/paper-jsdom/index.js
+++
@@ -1,3 +0,0 @@
-// Just pass through. The rest is handled by paper.js itself.
-// See `requireName` in paper.js/src/node/self.js
-module.exports = require('paper');
